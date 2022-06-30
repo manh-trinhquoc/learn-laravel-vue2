7834422ed8942cbe4e80bb052b4734830b8af34f
manhtq <trinhquocmanh1990@gmail.com>
2022-06-30 11:30:05 +0700

    123

---
+++ laravel/public/app.js
@@ -0,0 +1,25 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        title: sample.title,
+        address: sample.address,
+        about: sample.about,
+        headerImageStyle: {
+            'background-image': 'url(sample/header.jpg)'
+        },
+        amenities: sample.amenities,
+        prices: sample.prices,
+        contracted1: true,
+        modalOpen: false
+    },
+    watch: {
+        modalOpen: function () {
+            var className = 'modal-open';
+            if (this.modalOpen) {
+                document.body.classList.add(className);
+            } else {
+                document.body.classList.remove(className);
+            }
+        }
+    }
+});
