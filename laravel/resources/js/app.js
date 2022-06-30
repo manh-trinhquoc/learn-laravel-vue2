44a04248a4f2feb1c9835a8b545312df2a75cec6
manhtq <trinhquocmanh1990@gmail.com>
2022-06-30 16:10:54 +0700

    196 chap 5

--- laravel/resources/js/app.js
+++ laravel/resources/js/app.js
@@ -2,7 +2,7 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
-Vue.component('example', require('./components/Example.vue'));
+// Vue.component('example', require('./components/Example.vue'));
 
 const app = new Vue({
     el: '#app'
