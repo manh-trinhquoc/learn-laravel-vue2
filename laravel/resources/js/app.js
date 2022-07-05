4d3b5bd68042b575291807441f916f39abf23774
manhtq <trinhquocmanh1990@gmail.com>
2022-07-05 18:35:46 +0700

    330 chap 7

--- laravel/resources/js/app.js
+++ laravel/resources/js/app.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import "core-js/features/object/assign";
 
 import ListingPage from '../components/ListingPage.vue';
+import router from './router';
 
 var app = new Vue({
     el: '#app',
